5bace95cd9681a72520d621a55fbf6bdcfcbc67e
unknown <syahid@.(none)>
2013-11-16 15:55:27 +0700

    commit02

---
+++ src/Biodataku.java
@@ -0,0 +1,15 @@
+
+
+//Perintah penggunaan package versi 01
+package Latihan01.sesi1.bin;
+
+public class Biodataku
+{
+        public static void main(String[] data)
+        {
+                System.out.println("N a m a         : Syahid");
+                System.out.println("Alamat          : Bogor");
+                System.out.println("Golongan darah        : C");
+                System.out.println("Jenis Kelamin        : Laki-laki");
+        }
+}
