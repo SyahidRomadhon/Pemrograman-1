21f34d1a17b93525dd679e9fa2ce4fe5d7f44419
unknown <syahid@.(none)>
2013-11-20 20:59:23 +0700

    commit10

---
+++ src/Praktikum1/Person10.java
@@ -0,0 +1,25 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person10
+      {
+	   public static void main (String args[])
+	   {
+	      int num=11;
+		  if(num >5)
+		    System.out.println("Number is greater than 5");
+			   else
+			      System.out.println("Number is less Than 5");
+		}
+	   }
