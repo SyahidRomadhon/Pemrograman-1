ad106889ea31021ba3875a3153e6a2ab0aa06b50
unknown <syahid@.(none)>
2013-11-20 20:54:01 +0700

    commit6

---
+++ src/Praktikum1/Person6.java
@@ -0,0 +1,24 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+package Latihan04.sesi4.bin;
+
+public class Person6
+      {
+	   public static void main (String args[])
+	   {
+	      boolean test1;
+		  boolean test2;
+		  
+		  test1=(5 !=3);
+		  System.out.println(test1);
+		}
+	   }
