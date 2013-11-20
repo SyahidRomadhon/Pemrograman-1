c559f81e14cc3a3e199ae188422ae1928f44450a
unknown <syahid@.(none)>
2013-11-20 20:58:26 +0700

    commit9

---
+++ src/Praktikum1/Person9.java
@@ -0,0 +1,30 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person9
+      {
+	   public static void main (String args[])
+	   {
+	      int a=4,b=6;
+		  int num=a|b;
+		  if(num==4)
+		     System.out.println("value is 4");
+		  else if(num==5)
+		     System.out.println("value is 5");
+		  else if(num==6)
+		     System.out.println("value is 6");
+		  else if(num==7)
+		     System.out.println("value is 7");
+	    }
+	  }
