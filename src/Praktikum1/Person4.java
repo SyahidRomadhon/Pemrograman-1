4dcf0dc2e63ca94986c7adb88576a0590d104142
unknown <syahid@.(none)>
2013-11-20 20:50:00 +0700

    commit4

---
+++ src/Praktikum1/Person4.java
@@ -0,0 +1,30 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person4
+      {
+	   public static void main (String args[])
+	   {
+	     int x=10;
+		 
+		 if(x == 12)
+		 {
+		     System.out.println("x =12");
+		 }
+		 else
+		 {
+		     System.out.println("x not 12");
+		 }
+	   }
+	  }
