789aaf89c424989a3779c9d930ee6369ded40e35
unknown <syahid@.(none)>
2013-11-20 20:56:22 +0700

    commit7

---
+++ src/Praktikum1/Person7.java
@@ -0,0 +1,26 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+package Latihan04.sesi4.bin;
+
+public class Person7
+      {
+	   public static void main (String args[])
+	   {
+	     int firstNumber, secondNumber, results;
+		 
+		                  firstNumber =20;
+						  secondNumber=10;
+						  
+						  results=firstNumber + secondNumber;
+						  System.out.println(results);
+	    }
+	   }
