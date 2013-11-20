62f33e4f19fdff85aaf36e45138c735d31980472
unknown <syahid@.(none)>
2013-11-20 20:57:18 +0700

    commit8

---
+++ src/Praktikum1/Person8.java
@@ -0,0 +1,26 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person8
+      {
+	   public static void main (String args[])
+	    {
+		   int count =1;
+		   while(count <5)
+		   {
+		     System.out.println("Count is equal to " + count);
+			 count++;
+		   }
+		}
+	   }
