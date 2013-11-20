d3f79ef3425c2bca7b0980702619629aa353e1bf
unknown <syahid@.(none)>
2013-11-20 20:38:05 +0700

    commit2

---
+++ src/Praktikum1/Person2.java
@@ -0,0 +1,24 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person2
+      {
+	   public static void main (String args[])
+	   {
+	      for (int x = 0; x < 7; x++)
+		  {
+		    System.out.println("Helo, Worlds we are using loops");
+		  }
+	   }
+	  }
