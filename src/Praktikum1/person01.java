58a8ae54653429f38dc4f501dc69c0befb21b2f7
unknown <syahid@.(none)>
2013-11-20 20:29:06 +0700

    commit01

---
+++ src/Praktikum1/person01.java
@@ -0,0 +1,23 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 1
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan04.sesi4.bin;
+
+public class Person1
+{
+public static void main(String[] args)
+    {
+    int age = 21;
+	
+	System.out.println(age > 50 ? " You are old " : "You are Young");
+	}
+}
