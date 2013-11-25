5d21fc179c6bfad1fc9e882bcee4f96b30810a38
unknown <syahid@.(none)>
2013-11-25 19:36:11 +0700

    commit02

---
+++ src/Praktikum2/Latihan1.java
@@ -0,0 +1,28 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 2
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan05.sesi5.bin;
+
+public class Latihan1
+      {
+	     public static void main(String[]args){
+		 
+		    double tuna;
+			tuna=5.28;
+			
+			System.out.println("I want ");
+			System.out.println(tuna);
+			System.out.println("movies");
+			System.out.println("apples");
+			}
+			}
+      
