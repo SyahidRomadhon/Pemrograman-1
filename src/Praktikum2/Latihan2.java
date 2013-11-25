a5f3846ea40a3f0efaf81c153f4709c3b3e1e80f
unknown <syahid@.(none)>
2013-11-25 19:38:38 +0700

    commit03

---
+++ src/Praktikum2/Latihan2.java
@@ -0,0 +1,27 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 2
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan05.sesi5.bin;
+
+public class Latihan2
+      {
+	     public static void main(String[]args){
+		 
+		    int a;
+			a=10;
+			int b=20;
+			
+			System.out.println("a="+a);
+			System.out.println("b="+b);
+			System.out.println("a*b="+(a*b));
+		}
+	}
