4dfad6e90a9b191b9d3b89bac13d90d8083047ca
unknown <syahid@.(none)>
2013-11-25 19:41:07 +0700

    commit05

---
+++ src/Praktikum2/Latihan4.java
@@ -0,0 +1,37 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 2
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan05.sesi5.bin;
+
+public class Latihan4
+     {
+	   public static void main (String[]args){
+      
+	  byte number1=12;
+	  short number2=100;
+	  int number3=1000;
+	  long number4=20001;
+	  float number5=120023.45f;
+	  double number6=123456;
+	  boolean value=true;
+	  char character='a';
+      
+	  System.out.println(number1);
+	  System.out.println(number2);
+	  System.out.println(number3);
+	  System.out.println(number4);
+	  System.out.println(number5);
+	  System.out.println(number6);
+	  System.out.println(value);
+	  System.out.println(character);
+	  }
+	  }
