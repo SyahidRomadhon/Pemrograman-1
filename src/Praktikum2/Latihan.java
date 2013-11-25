5783434845214a1ddad6b15be42b871340c21fc5
unknown <syahid@.(none)>
2013-11-25 18:39:50 +0700

    commit01

---
+++ src/Praktikum2/Latihan.java
@@ -0,0 +1,28 @@
+/*
+Operator
+Control Flow ( if-else, for/while)
+====================================
+Tugas Praktikum 2
+Nama      : Syahid
+Jurusan   : Teknik Informatika
+Studi     : Pemrograman 1
+No.Pokok  : 4512215052
+====================================
+*/
+
+package Latihan05.sesi5.bin;
+
+public class Latihan
+      {
+	     public static void main(String[]args){
+		 
+		    int favNum;
+			String words;
+			
+			favNum=7;
+			words="My favorite number is";
+			
+			System.out.println(words+favNum+".");
+		}
+	}
+      
