86ce40544b8ebebdc1cd1be6ba3df8ceb41426e2
unknown <syahid@.(none)>
2014-01-11 06:28:22 -0800

    commit13

---
+++ src/Praktikum3/LatihanD.java
@@ -0,0 +1,19 @@
+/* 
+Praktikum III
+Nama    :  Nur Aulia Syahid Romadhon
+NIM     :  4512215052
+No.soal :  03
+Jurusan :  Teknik Informatika
+*/
+
+package Latihan03.sesi3.bin;
+public class LatihanD
+{
+private Runnable thread = new Runnable()
+{
+public void run()
+{
+System.out.println("Hello World from Thread");
+}
+};
+}
