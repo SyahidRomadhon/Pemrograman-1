3d6fc117b4f1e5c6ec5fe6ea85a32a0e78ca3d51
unknown <syahid@.(none)>
2014-01-11 06:32:59 -0800

    commit13

---
+++ src/Praktikum3/LatihanE.java
@@ -0,0 +1,30 @@
+/* 
+Praktikum III
+Nama    :  Nur Aulia Syahid Romadhon
+NIM     :  4512215052
+No.soal :  05
+Jurusan : Teknik Informatika
+*/
+
+package Latihan03.sesi3.bin;
+public class LatihanE
+{
+private Long id;
+private String nama;
+public String getNama()
+{
+return nama;
+}
+public void setNama(String nm)
+{
+nama = nm;
+}
+public Long getId()
+{
+return id;
+}
+public void setId(Long i)
+{
+id = i;
+}
+}
