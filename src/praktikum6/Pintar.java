70bec317b2ea4a03971a4368bbb495f91dc4182a
unknown <syahid@.(none)>
2014-01-11 07:44:12 -0800

    commit23

---
+++ src/praktikum6/Pintar.java
@@ -0,0 +1,11 @@
+//Perintah Inheritance praktikum 6
+package Latihan06.sesi6.bin;
+public class Pintar
+{
+public static void main(String[] args)
+{
+Belajar panggil = new Belajar();
+System.out.println("Panggil X : " +panggil.x);
+System.out.println("Panggil Y : " +panggil.y);
+}
+}
